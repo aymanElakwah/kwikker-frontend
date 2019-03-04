cc3bbcecc34a357689a598f67c9fbffb4b446b2d
phella <philopateernabil2021@gmail.com>
2019-03-05 00:23:58 +0200

    handle arabic search

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -8,10 +8,14 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class SearchComponent implements OnInit {
   search: string ;
+  arabic = /[\u0600-\u06FF]/; // arabic range of unicode
+   ltr: boolean;
   constructor(private route: ActivatedRoute) { }
-
   ngOnInit() {
+    this.ltr = true;
     this.search = this.route.snapshot.queryParamMap.get('filterBy');
+    if (this.arabic.test(this.search)) {
+      this.ltr = false;
+    }
   }
-
 }
