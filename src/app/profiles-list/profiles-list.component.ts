9dc4dba70bb890159ceda9d35a888d8e9a749c94
phella <philopateernabil2021@gmail.com>
2019-03-07 01:11:04 +0200

    creating profile list and adding to shared module

---
+++ src/app/profiles-list/profiles-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profiles-list',
+  templateUrl: './profiles-list.component.html',
+  styleUrls: ['./profiles-list.component.css']
+})
+export class ProfilesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { ProfilesListComponent } from '../profiles-list/profiles-list.component';
+import { KweekComponent } from '../kweek/kweek.component';
 
 @NgModule({
-  declarations: [NavBarComponent],
+  declarations: [NavBarComponent,
+  ProfilesListComponent,
+  KweekComponent],
   imports: [
     CommonModule
   ],
-  exports: [NavBarComponent]
+  exports: [NavBarComponent,
+  ProfilesListComponent,
+  KweekComponent]
 })
 export class SharedModule { }
