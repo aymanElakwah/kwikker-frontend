caae72aacb44dba1f69176affc6e91dda5a46901
phella <philopateernabil2021@gmail.com>
2019-03-07 01:16:31 +0200

    adding children routes

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule  } from '@angular/common';
 import { SearchComponent } from './search.component';
 import { SharedModule } from '../shared/shared.module';
-
+import { RouterModule } from '@angular/router';
+import { KweekComponent } from '../kweek/kweek.component';
+import { ProfilesListComponent } from '../profiles-list/profiles-list.component';
 @NgModule({
   declarations: [SearchComponent],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    RouterModule.forChild([
+      {path: 'search', component: SearchComponent ,
+    children: [
+      {path: 'kweeks', component: KweekComponent },
+      {path: 'people', component: ProfilesListComponent}
+    ]}
+    ])
   ]
 })
 export class SearchModule { }
