1026eb7adb43e8f99355ee52dc2d61a423c77684
phella <philopateernabil2021@gmail.com>
2019-03-06 19:33:18 +0200

    solving navbar component error

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from './search.component';
 import { SharedModule } from '../shared/shared.module';
-import { NavBarComponent } from '../nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [SearchComponent,
-  NavBarComponent],
+  declarations: [SearchComponent],
   imports: [
     CommonModule,
     SharedModule
