9fcac517e3b97af9f2c275057116334cb484c5af
phella <philopateernabil2021@gmail.com>
2019-03-07 17:24:20 +0200

    generate who to follow component and add it to shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,16 +3,20 @@ import { CommonModule } from '@angular/common';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { ProfilesListComponent } from '../profiles-list/profiles-list.component';
 import { KweekComponent } from '../kweek/kweek.component';
+import { WhoToFollowComponent } from '../who-to-follow/who-to-follow.component';
 
 @NgModule({
   declarations: [NavBarComponent,
   ProfilesListComponent,
-  KweekComponent],
+  KweekComponent,
+  WhoToFollowComponent],
   imports: [
     CommonModule
   ],
-  exports: [NavBarComponent,
+  exports: [
+  NavBarComponent,
   ProfilesListComponent,
-  KweekComponent]
+  KweekComponent,
+  WhoToFollowComponent]
 })
 export class SharedModule { }
---
+++ src/app/who-to-follow/who-to-follow.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-who-to-follow',
+  templateUrl: './who-to-follow.component.html',
+  styleUrls: ['./who-to-follow.component.css']
+})
+export class WhoToFollowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
