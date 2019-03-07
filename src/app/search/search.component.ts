b1c664736719352439dc456163d582f99ee8e560
phella <philopateernabil2021@gmail.com>
2019-03-07 02:55:39 +0200

    prevent empty string in searching

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -10,11 +10,13 @@ export class SearchComponent implements OnInit {
   search: string ;
   arabic = /[\u0600-\u06FF]/; // arabic range of unicode
    ltr: boolean;
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
   ngOnInit() {
     this.ltr = true;
     this.search = this.route.snapshot.queryParamMap.get('filterBy');
-    if (this.arabic.test(this.search)) {
+    if ( this.search === '' || this.search === null ) {
+      this.router.navigate(['/home']);
+    } else if (this.arabic.test(this.search)) {
       this.ltr = false;
     }
   }
