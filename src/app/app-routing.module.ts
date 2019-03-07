2e5b13c2d801a464b754be911196f2fb2fdbbd69
phella <philopateernabil2021@gmail.com>
2019-03-07 02:39:17 +0200

    adding home path and removing search path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SearchComponent } from './search/search.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: 'search', component: SearchComponent }
+  {path: 'home', component: HomeComponent}
 ];
 
 @NgModule({
