3e8c59b4e4525fbda82dba4f10106451cd23896b
Yousif Ahmed <44294341+YousifGamal@users.noreply.github.com>
2019-03-17 00:43:10 +0200

    solving conflicts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,31 +7,17 @@ import { NotificationsComponent } from './notifications/notifications.component'
 
 
 
-<<<<<<< HEAD
 
-
-const ROUTES: Routes = [
-  {path: 'home', component: HomeComponent}
-||||||| merged common ancestors
-const routes: Routes = [
-  {path: 'home', component: HomeComponent}
-=======
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path:'notifications', component: NotificationsComponent}
->>>>>>> jimmy
 ];
 
 @NgModule({
-<<<<<<< HEAD
-  imports: [RouterModule.forRoot(ROUTES)],
-||||||| merged common ancestors
-  imports: [RouterModule.forRoot(routes)],
-=======
+
   imports: [RouterModule.forRoot(routes),
     NotificationsModule
   ],
->>>>>>> jimmy
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
