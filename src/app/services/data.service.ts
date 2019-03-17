fc6627b71cb950159cff759726283ee63c48c98e
philopateer <philopateer.elmaseh98@eng-st.cu.edu.eg>
2019-03-17 19:06:21 +0200

    get conversations from backend

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpErrorResponse, HttpClient, HttpParams } from '@angular/common/http';
 import { throwError , Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { Conversation } from '../model/inbox';
+import { BrowserStack } from 'protractor/built/driverProviders';
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-
+  base = ' ';
   constructor(private http: HttpClient) { }
 
   // example for using get with jason file
@@ -19,6 +21,11 @@ export class DataService {
       );
   }
 
+  getConverstations(): Observable<Conversation[]> {
+      return this.http.get<Conversation[]>(`${this.base}direct_message/conversations `).pipe(
+        catchError(this.handleError)
+      );
+  }
   // use retry func
   private handleError(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
@@ -27,6 +34,14 @@ export class DataService {
     } else {
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong,
+      switch ( error.status)  {
+      case(404):
+        console.error('not found');
+        break ;
+        case(401):
+          console.error('unauthorized');
+        break ;
+      }
       console.error(
         `Backend returned code ${error.status}, ` +
         `body was: ${error.error}`);
