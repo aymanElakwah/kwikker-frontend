15729a0c8b235e58b2e68f039bc256a6d64a0f3a
philopateer <philopateer.elmaseh98@eng-st.cu.edu.eg>
2019-03-20 22:26:04 +0200

    setting tokens in the head of the request

--- src/app/services/add-token.interceptor.ts
+++ src/app/services/add-token.interceptor.ts
@@ -3,9 +3,11 @@ import {HttpInterceptor , HttpRequest , HttpHandler , HttpEvent} from '@angular/
 import {Observable} from 'rxjs';
 @Injectable( )
 export class AddTokenInterceptor implements HttpInterceptor {
+    token: string;
     intercept(req: HttpRequest<any>, next: HttpHandler ): Observable<HttpEvent<any>> {
+        this.token = localStorage.getItem('token');
         const jsonReq: HttpRequest<any> = req.clone({
-            setHeaders: {'Content-Type': 'application/json' , 'tokens': '21312312'}
+            setHeaders: {'Content-Type': 'application/json' , 'tokens': `${this.token}` }
         });
         return next.handle(jsonReq);
     }
