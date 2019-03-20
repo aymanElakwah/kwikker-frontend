4c70a72036ba4132090b8ff6a867a41b58e36d40
philopateer <philopateer.elmaseh98@eng-st.cu.edu.eg>
2019-03-20 22:32:07 +0200

    new inbox list component

---
+++ src/app/inbox-list/inbox-list.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InboxListComponent } from './inbox-list.component';
+
+describe('InboxListComponent', () => {
+  let component: InboxListComponent;
+  let fixture: ComponentFixture<InboxListComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ InboxListComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InboxListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
