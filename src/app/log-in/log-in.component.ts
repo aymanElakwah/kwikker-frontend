bf27b8e306d49f01699dfb850b8c8ca3018d2c13
philopateer <philopateer.elmaseh98@eng-st.cu.edu.eg>
2019-03-21 21:50:56 +0200

    store the username

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -10,23 +10,20 @@ import {LogInModule} from './log-in.module';
   styleUrls: ['./log-in.component.css']
 })
 export class LogInComponent implements OnInit {
- 
   public mail: string;
   public pass: string;
   public isLoggedIn: boolean;
- 
-  constructor(private data: DataService ,private router:Router) {
-   
+  constructor(private data: DataService , private router: Router) {
 
    }
 /**
-   * 
+   *
    * to check for user's information when logging in, and then send it to be posted in the backend
    * @param form {NgForm} it calls logInUser(user) to verify the logged in user and gets 
    * token after it and also could be null
    * @returns void
    */
-  submitForm(form: NgForm){
+  submitForm(form: NgForm) {
     this.isLoggedIn = false;
     // before submitting anyhting, we need to validate the inputs
     if ( this.mail.length < 2 ) {
@@ -40,13 +37,12 @@ export class LogInComponent implements OnInit {
        res => {
          console.log(res);
          localStorage.setItem('TOKEN', res.token);
+         localStorage.setItem('username', this.mail);
          this.router.navigate(['/home']);
        },
-        err=> console.log('error: ',err)
-      )
-      
+        err => console.log('error: ', err)
+    );
     this.isLoggedIn = true;
-    
   }
   ngOnInit() {
   }
