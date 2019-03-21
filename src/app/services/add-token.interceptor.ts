1ba5f153bc24ee92cca761e8a7050402bbd43709
philopateer <philopateer.elmaseh98@eng-st.cu.edu.eg>
2019-03-21 17:52:47 +0200

    discarding some requests from adding tokens

--- src/app/services/add-token.interceptor.ts
+++ src/app/services/add-token.interceptor.ts
@@ -6,6 +6,9 @@ export class AddTokenInterceptor implements HttpInterceptor {
     token: string;
     intercept(req: HttpRequest<any>, next: HttpHandler ): Observable<HttpEvent<any>> {
         this.token = localStorage.getItem('TOKEN');
+        if ( req.url === 'http://0d977716.ngrok.io/account/login') {
+            return next.handle(req);
+        }
         const jsonReq: HttpRequest<any> = req.clone({
             setHeaders: {'Content-Type': 'application/json' , 'TOKEN': `${this.token}` }
         });
