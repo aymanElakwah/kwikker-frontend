be428d822265fb943e4aa267b46c0a651cc85e13
AhmedAfifi98 <Ahmed.Afifi98@eng-st.cu.edu.eg>
2019-03-24 01:48:51 +0200

    Document All Models

--- src/app/model/mention.ts
+++ src/app/model/mention.ts
@@ -1,3 +1,8 @@
+
+/**
+ * Mention Model, This Model is used Through Kweeks when The useer wants
+ * to mention another user in his kweeks 
+ *  */
 export interface Mention {
     username: string;
     indices: number[];
