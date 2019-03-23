10b55172b1c503ae45eaa7ec0e64ac44807f4d84
philopateer <philopateer.elmaseh98@eng-st.cu.edu.eg>
2019-03-23 22:53:21 +0200

    arabic search should flow right

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -14,12 +14,15 @@ export class SearchComponent implements OnInit {
   constructor(private route: ActivatedRoute, private router: Router, private tabService: TitleService) { }
   ngOnInit() {
     this.ltr = true;
+    this.testSearchingParams();
+    this.tabService.setTitleSearch(this.search);
+  }
+  testSearchingParams(): void {
     this.search = this.route.snapshot.queryParamMap.get('filterBy');
     if ( this.search === '' || this.search === null ) {
       this.router.navigate(['/home']);
     } else if (this.arabic.test(this.search)) {
       this.ltr = false;
     }
-    this.tabService.setTitleSearch(this.search);
   }
 }
