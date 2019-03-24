72243b963895542715c92a5d98b55861e147dc6c
philopateer <philopateer.elmaseh98@eng-st.cu.edu.eg>
2019-03-24 02:06:52 +0200

    search unit testing

--- src/app/search/search.component.spec.ts
+++ src/app/search/search.component.spec.ts
@@ -1,25 +1,53 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SearchComponent } from './search.component';
+import { Router } from '@angular/router';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Route } from '@angular/compiler/src/core';
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
-
+  // tslint:disable-next-line:prefer-const
+  let router: Router;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SearchComponent ]
+      imports: [
+        RouterTestingModule.withRoutes([
+        {path: 'home' , component: SearchComponent
+        }])
+      ],
+      declarations: [ SearchComponent,
+      NavBarComponent ],
     })
     .compileComponents();
   }));
-
+  router = TestBed.get(Router);
   beforeEach(() => {
     fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    let mockservice;
+    const comp = new SearchComponent(mockservice, mockservice, mockservice);
+    mockservice = jasmine.createSpyObj(['route']);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should detect arabic serach', () => {
+    component.search = 'عربي';
+    component.arabic = /[\u0600-\u06FF]/;
+    component.testSearchingParams();
+    expect(component.ltr).toBe(false);
+  });
+
+  it('should only work with right parameters', () => {
+    const navigateSpy = spyOn(router, 'navigate');
+    component.search = '';
+    component.testSearchingParams();
+    expect(navigateSpy).toHaveBeenCalledWith(['/home']);
+  });
+
 });
