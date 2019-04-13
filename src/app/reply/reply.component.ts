d04822c0c9a0a207ed64d0805b27567927d1c221
Abdulrahman Khalid <abdulrahman.khalid@bk.ru>
2019-04-13 17:58:30 +0200

    more styling and scrollbar added to the popups

--- src/app/reply/reply.component.ts
+++ src/app/reply/reply.component.ts
@@ -1,10 +1,3 @@
-// import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-// import { DataService } from '../services/data.service';
-// import { Kweek } from '../model/kweek';
-// import { MatDialog, MatDialogConfig, MAT_DIALOG_DATA, MatDialogRef, TooltipPosition } from '@angular/material';
-// import { FormControl } from '@angular/forms';
-// import { KweeksService } from '../services/kweeks.service';
-
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { DataService } from '../services/data.service';
 import { Kweek } from '../model/kweek';
@@ -12,14 +5,16 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { MatDialog, MatDialogConfig, MatDialogRef, TooltipPosition } from '@angular/material';
 import { FormControl } from '@angular/forms';
 import { KweeksService } from '../services/kweeks.service'  ;
+import { Overlay } from '@angular/cdk/overlay';
 @Component({
   selector: 'app-reply',
   templateUrl: './reply.component.html',
-  styleUrls: ['../kweek/kweek.component.css', './reply.component.css' ],
+  styleUrls: ['./reply.component.css', '../kweek/kweek.component.css'],
   encapsulation: ViewEncapsulation.None,
 })
 export class ReplyComponent implements OnInit {
   roots: Kweek[] = [];
+  clickedKweek: Kweek;
   replies: Kweek[] = [];
   positionOption: TooltipPosition = 'above';
   position = new FormControl(this.positionOption);
@@ -28,7 +23,8 @@ export class ReplyComponent implements OnInit {
   constructor(public dialogRef: MatDialogRef<ReplyComponent>,
     private kweekService: DataService,
     private kweekFunc: KweeksService,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private overlay: Overlay
     ) { }
 
   ngOnInit() {
@@ -39,15 +35,18 @@ export class ReplyComponent implements OnInit {
   }
 
   nestedDialog(kweek: Kweek): void {
-    this.roots.push(kweek);
+    this.roots.push(this.clickedKweek);
     const dialogConfig = new MatDialogConfig();
     dialogConfig.width = '640px';
     dialogConfig.autoFocus = false;
+    // dialogConfig.scrollStrategy = this.overlay.scrollStrategies.reposition();
     const dialogRef = this.dialog.open(ReplyComponent, dialogConfig);
     dialogRef.componentInstance.roots = this.roots;
+    dialogRef.componentInstance.clickedKweek = kweek;
     this.dialogRef.close();
     dialogRef.afterClosed().subscribe(result => {
       this.roots = [];
+      this.clickedKweek = null;
     });
   }
 
--- src/app/services/in-memory-data-service.service.ts
+++ src/app/services/in-memory-data-service.service.ts
@@ -292,6 +292,35 @@ export class InMemoryDataService {
           'liked_by_user': true,
           'rekweeked_by_user': true
         },
+        {
+          'id': '3',
+          'created_at': '2011-04-11T10:20:30Z',
+          'text': 'hello world',
+          'media_url': 'https://media.idownloadblog.com/wp-content/uploads/2017/03/Twitter-new-2017-avatar-001.png',
+          'user': {
+            'username': 'ayy_abdulrahman',
+            'screen_name': 'Abdulrahman Khalid',
+            'profile_image_url': 'https://pbs.twimg.com/profile_images/1030601937115381760/tYVfVdN__400x400.jpg',
+            'following': true,
+            'follows_you': true,
+            'blocked': false,
+            'muted': false
+          },
+          'mentions': [
+          ],
+          'hashtags': [
+          ],
+          'number_of_likes': 1,
+          'number_of_rekweeks': 2,
+          'number_of_replies': 3,
+          'reply_to': 'string',
+          'rekweek_info': {
+            'rekweeker_name': 'string',
+            'rekweeker_username': 'string'
+          },
+          'liked_by_user': true,
+          'rekweeked_by_user': true
+        },
     ];
 
     const REPLY1 = [ {
