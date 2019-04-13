97ca60daf9468edd97086b8d25ad118911dc4202
philopateer <philopateer.elmaseh98@eng-st.cu.edu.eg>
2019-04-13 20:17:31 +0200

    inbox model updated

--- src/app/model/inbox.ts
+++ src/app/model/inbox.ts
@@ -1,9 +1,9 @@
 import { MiniUser } from './mini-user';
-import { Message } from '@angular/compiler/src/i18n/i18n_ast';
+import { Message } from '../model/message';
 /**
  * we get a list of last conversations when openning messages
  */
 export interface Conversation {
     user: MiniUser;
-    lsat_message: Message;
+    last_message: Message;
 }
