101dc6c69017fbfb89c6e7ea213f83d1190cf30e
Evram <evramyousef@gmail.com>
2019-04-21 04:41:49 +0200

    Redesigining the navbar inorder to make it responsive

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -3,6 +3,7 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { MatDialog } from '@angular/material';
 import { NewKweekComponent } from '../new-kweek/new-kweek.component';
 import { isNull } from 'util';
+import { InboxComponent } from '../inbox/inbox.component';
 
 @Component({
   selector: 'app-nav-bar',
@@ -43,5 +44,28 @@ export class NavBarComponent implements OnInit {
 
     console.log("modal should show");
   }
+   /**
+   * Function to open kweek dialog 
+   * paneClass -> attach the dialog to specific css class
+   */
 
+  openInboxComponent(){
+    console.log("working")
+    const dialogRef = this.dialog.open(InboxComponent, {
+      height: '600px',
+      width: '1000px',
+    });
+  
+  /**
+   * Function for closing the dialog and displaying a msg 
+   * 
+   */
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+
+    console.log("modal should show");
+  }
+  
 }
