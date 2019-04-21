ff71407ee9f34d72f1e38dc00904052c0182dd26
philopateer <philopateer.elmaseh98@eng-st.cu.edu.eg>
2019-04-21 15:46:38 +0200

    nav bar search

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -18,43 +18,8 @@ import { ENTER, COMMA } from '@angular/cdk/keycodes';
 })
 export class NavBarComponent implements OnInit {
   public userName: string;
- /**
-   * addressed person
-   */
-  addressee: MiniUser;
-  /**
-   * creating message form
-   */
-  myForm2: FormGroup;
-  /**
-   * if message conatin image add div
-   */
-  uploadImg = false;
-  /**
-   * image url if uploaded
-   */
-  imgURL: any;
-  /**
-   * filtered list of users
-   */
-  users: MiniUser[];
-  /**
-   * selected users list to send message
-   */
-  selectedUsers: string[] = [];
-  /**
-   * reactive form
-   */
-  myForm: FormGroup;
-  /**
-   * user selected addresses
-   */
-  secondStep=false;
-  visible = true;
-  selectable = true;
-  removable = true;
-  addOnBlur = true;
-  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  users:MiniUser[];
+ filterBy:string;
 
 
 
@@ -76,18 +41,6 @@ export class NavBarComponent implements OnInit {
     {
       this.userName = "user"; 
     }
-    this.myForm = this.fb.group({
-      filterBy: ['']
-    });
-    this.myForm2 = this.fb.group({
-      message: ['', [
-        Validators.required
-      ]],
-      file : [null]
-      });
-    this.data.getRecentConversations().subscribe(users => {
-      this.users = users.slice(0,7); }
-      );
   }
 
   /**
@@ -148,116 +101,16 @@ export class NavBarComponent implements OnInit {
     this.router.navigate(["/"]);
   }
   /**
-   * after each char send new request
+   * after each char send new request or navigate
    */
-  newSearch() {
-    this.data.searchUsers(this.myForm.controls.filterBy.value).subscribe(
+  newSearch(event) {
+    if(event.key === "Enter") {
+      // navigate to search
+    }
+    this.data.searchUsers(this.filterBy).subscribe(
       list => { this.users = list; }
     );
   }
-  /**
-   * remove one of selected users
-   * @param user selected user to get removed
-   */
-  remove(user) {
-    const index = this.selectedUsers.indexOf(user);
-    if (index >= 0) {
-      this.selectedUsers.splice(index, 1);
-  }
-}
-/**
- * on enter add user
- * @param event keyboard shortcuts event
- */
-add(event: MatChipInputEvent): void {
-  const input = event.input;
-  let found = false;
-  // tslint:disable-next-line:no-shadowed-variable
-  this.selectedUsers.forEach(element => {
-    if ( this.users[0].username === element) {
-        found = true;
-    }
-  }
-  );
-  if ( found) {
-    return;
-  }
-  if (this.users.length > 0) {
-    this.selectedUsers.push( this.users[0].username);
-  }
-  // Reset the input value
-  if (input) {
-    input.value = '';
-  }
-}
-/**
- * select user by click
- * @param user user that get clicked on
- */
-  selectUser(user: MiniUser) {
-    let flag = false;
-    // tslint:disable-next-line:no-shadowed-variable
-    this.selectedUsers.forEach(element => {
-      if ( user.username === element) {
-          flag = true;
-      }
-    }
-    );
-    if ( flag) {
-      return;
-    }
-    this.selectedUsers.push(user.username);
-  }
 
-
-  /**
-   * click input tag on button click
-   */
-  upload() {
-    const element: HTMLElement = document.getElementById('fileInput') as HTMLElement;
-    element.click();
-  }
-  /**
-   * preview images after uploading it
-   * @param files uploaded images
-   */
-  uploadIamge(files) {
-    this.uploadImg = true;
-    const reader = new FileReader();
-    const imagePath = files;
-    reader.readAsDataURL(files[0]);
-    reader.onload = (_event) => {
-      this.imgURL = reader.result;
-    };
-  }
-  /**
-   * remove image from uploading it
-   */
-  removeImg() {
-    this.uploadImg = false;
-    this.myForm2.patchValue({
-      file: null
-    });
-  }
-  /**
-   * send current message
-   */
-  send() {
-    const message = {
-      text: '',
-      username: '',
-      media_url: ''
-    };
-    message.text = this.myForm2.controls.message.value;
-    message.media_url = '' ;
-    this.selectedUsers.forEach(element => {
-      message.username = element;
-      this.data.createMessage(message).subscribe();
-    });
-    this.router.navigate(['/chat/', {outlets : {body: ['inbox']} }]);
-  }
-  next(){
-    this.secondStep = true;
-  }
-  
 }
+
