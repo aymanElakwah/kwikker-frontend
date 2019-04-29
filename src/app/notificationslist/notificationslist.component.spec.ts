b7e3ee4896ebe2c810ac42d86a52e44fdd81b1c8
YousifGamal <YousifGAhmed@gmail.com>
2019-04-30 00:54:22 +0200

    notifications list unit testing

--- src/app/notificationslist/notificationslist.component.spec.ts
+++ src/app/notificationslist/notificationslist.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NotificationslistComponent } from './notificationslist.component';
 import { DataService } from '../services/data.service';
+import { empty, from } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 describe('NotificationslistComponent', () => {
   let component: NotificationslistComponent;
@@ -9,6 +11,7 @@ describe('NotificationslistComponent', () => {
   let notification_service:DataService;
 
   beforeEach(() => {
+    notification_service = new DataService(null,null);
     component = new NotificationslistComponent(notification_service);
   
   });
@@ -16,4 +19,51 @@ describe('NotificationslistComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe("notifications functionality",()=>{
+    let notificArr = {
+      unseen_count: Number(),
+      Notifications:[{
+        id:String()
+      }]
+    }
+    it("ngOnInit should request list of notifications",()=>{
+      let spy = spyOn(notification_service,"getNotificationsList").and.callFake(()=>{
+        return empty();
+      })
+
+      component.ngOnInit();
+      expect(spy).toHaveBeenCalled();
+    })
+
+    it("refreshNotifications should request list of notifications",()=>{
+      let spy = spyOn(notification_service,"getNotificationsList").and.callFake(()=>{
+        return empty();
+      })
+
+      component.refreshNotifications();
+      expect(spy).toHaveBeenCalled();
+    })
+
+    it("color changes",()=>{
+      component.new_count = 2;
+      let new_color = component.getColor(1);
+      expect(new_color).toBe("#E8F5FD");
+      new_color = component.getColor(2);
+      expect(new_color).toBe("white"); 
+    })
+
+    it("should call on scroll",()=>{
+      notificArr.unseen_count = 2;
+      notificArr.Notifications = [{id:"1"},{id:"2"}];
+      component.lastSeen = "1";
+      let spy = spyOn(notification_service,"getNotificationsList").and.callFake(()=>{
+        return from([notificArr]);
+      })
+      component.ngOnInit();
+      component.onScroll();
+      expect(spy).toHaveBeenCalled();
+    })
+
+  })
 });
